434602555d7e7de092da1028a3f3d069d1bbda95
kyle Dornan <kyledornan26@gmail.com>
2020-06-05 13:16:43 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,10 @@
 class Dog 
-  def initialize (name) 
+def initialize (name)
   @name = name 
+end 
+def name=(name)
+@name = name 
 end 
 def name 
   @name 
-end 
-
-def initialize(breed)
-  @breed = breed 
-end 
-def breed 
-  @breed 
-end 
-
 end 
